a6a202d78606c32f5573d96de1a4a2b9a5ddec2f
craftysnatcher <millenium@newaddr.com>
2025-05-28 19:10:28 +0930

    Updated Main.pas

--- Main.pas
+++ Main.pas
@@ -9,12 +9,30 @@ uses
   Vcl.VirtualImageList, Vcl.Imaging.pngimage, scStyledForm, scGPExtControls,
   scGPPagers, scGPImages, Vcl.StdCtrls, IdBaseComponent, IdComponent,
   IdCustomTCPServer, IdTCPServer, Vcl.ComCtrls, System.Generics.Collections,
-  IdHTTP, IdTCPConnection, IdTCPClient, IdURI,
+  IdHTTP, IdTCPConnection, IdTCPClient, IdURI, WinSock,
   IdSSLOpenSSL, IdSSL, System.JSON, System.IOUtils, System.UITypes, Math, IdContext, IdGlobal,
   Vcl.Menus, Winapi.CommCtrl, DzSocket, UClient, Winapi.MMSystem,
   scModernControls, Vcl.Mask, System.NetEncoding, Winapi.ShellAPI;
 
 type
+  TPacketInfo = record
+    Timestamp: TDateTime;
+    SourceIP: string;
+    DestIP: string;
+    Protocol: string;
+    Size: Integer;
+    Data: TBytes;
+  end;
+
+  TNetworkStats = record
+    BytesSent: Int64;
+    BytesReceived: Int64;
+    PacketsSent: Int64;
+    PacketsReceived: Int64;
+    AverageLatency: Double;
+    PacketLoss: Double;
+  end;
+
   TForm2 = class(TForm)
     scGPPanel23: TscGPPanel;
     lblTotalConnections: TLabel;
@@ -31,15 +49,32 @@ type
     lblMemoryUsage: TLabel;
     tmrStats: TTimer;
     
+    lvPackets: TListView;
+    scGPPanel26: TscGPPanel;
+    lblNetworkStats: TLabel;
+    scGPChart2: TscGPChart;
+    tmrPacketCapture: TTimer;
+    
     procedure FormCreate(Sender: TObject);
     procedure tmrStatsTimer(Sender: TObject);
+    procedure tmrPacketCaptureTimer(Sender: TObject);
+    procedure SClientRead(Sender: TObject; Socket: TDzSocket; const Cmd: Char; const A: string);
     
   private
     FPeakConnections: Integer;
     FStartTime: TDateTime;
+    FPacketLog: TList<TPacketInfo>;
+    FNetworkStats: TNetworkStats;
+    
     procedure UpdateStatistics;
     procedure UpdateGeographicalData;
     procedure UpdateSystemHealth;
+    procedure UpdatePacketMonitoring;
+    procedure LogPacket(const PacketInfo: TPacketInfo);
+    function CalculateNetworkStats: TNetworkStats;
+    procedure InitializePacketCapture;
+    function GetCPUUsage: Integer;
+    function ExtractCountryFromNickname(const Nickname: string): string;
   end;
 
 var
@@ -55,6 +90,9 @@ begin
   
   FPeakConnections := 0;
   FStartTime := Now;
+  FPacketLog := TList<TPacketInfo>.Create;
+  
+  InitializePacketCapture;
   
   with lvGeography do
   begin
@@ -62,6 +100,16 @@ begin
     Columns.Add.Caption := 'Country';
     Columns.Add.Caption := 'Connections';
     Columns.Add.Caption := 'Percentage';
+  end;
+  
+  with lvPackets do
+  begin
+    ViewStyle := vsReport;
+    Columns.Add.Caption := 'Time';
+    Columns.Add.Caption := 'Source';
+    Columns.Add.Caption := 'Destination';
+    Columns.Add.Caption := 'Protocol';
+    Columns.Add.Caption := 'Size';
   end;
   
   pbCPUUsage.Min := 0;
@@ -72,13 +120,21 @@ begin
   tmrStats.Interval := 1000;
   tmrStats.Enabled := True;
   
+  tmrPacketCapture.Interval := 100;
+  tmrPacketCapture.Enabled := True;
+  
   scGPChart1.Title := 'Connection History';
   scGPChart1.XAxisTitle := 'Time';
   scGPChart1.YAxisTitle := 'Connections';
   
+  scGPChart2.Title := 'Bandwidth Usage';
+  scGPChart2.XAxisTitle := 'Time';
+  scGPChart2.YAxisTitle := 'KB/s';
+  
   scGPPanel23.FillColor := $2D2D30;
   scGPPanel24.FillColor := $2D2D30;
   scGPPanel25.FillColor := $2D2D30;
+  scGPPanel26.FillColor := $2D2D30;
   
   scGPPageControl1.TabIndex := 1;
   SetupListView;
@@ -90,6 +146,11 @@ begin
   UpdateStatistics;
   UpdateGeographicalData;
   UpdateSystemHealth;
+end;
+
+procedure TForm2.tmrPacketCaptureTimer(Sender: TObject);
+begin
+  UpdatePacketMonitoring;
 end;
 
 procedure TForm2.UpdateStatistics;
@@ -165,6 +226,95 @@ begin
   
   pbCPUUsage.Position := GetCPUUsage;
   lblCPUUsage.Caption := Format('CPU Usage: %d%%', [pbCPUUsage.Position]);
+end;
+
+procedure TForm2.UpdatePacketMonitoring;
+var
+  Stats: TNetworkStats;
+  Item: TListItem;
+begin
+  Stats := CalculateNetworkStats;
+  
+  lblNetworkStats.Caption := Format(
+    'Network Statistics'#13#10 +
+    'Bytes Sent: %d'#13#10 +
+    'Bytes Received: %d'#13#10 +
+    'Packets Sent: %d'#13#10 +
+    'Packets Received: %d'#13#10 +
+    'Average Latency: %.2f ms'#13#10 +
+    'Packet Loss: %.2f%%',
+    [Stats.BytesSent, Stats.BytesReceived, Stats.PacketsSent,
+     Stats.PacketsReceived, Stats.AverageLatency, Stats.PacketLoss]
+  );
+  
+  scGPChart2.AddPoint(
+    (Stats.BytesSent + Stats.BytesReceived) / 1024
+  );
+  
+  if FPacketLog.Count > 1000 then
+    FPacketLog.Delete(0);
+    
+  while lvPackets.Items.Count > 100 do
+    lvPackets.Items.Delete(0);
+end;
+
+procedure TForm2.LogPacket(const PacketInfo: TPacketInfo);
+var
+  Item: TListItem;
+begin
+  FPacketLog.Add(PacketInfo);
+  
+  Item := lvPackets.Items.Insert(0);
+  Item.Caption := FormatDateTime('hh:nn:ss.zzz', PacketInfo.Timestamp);
+  Item.SubItems.Add(PacketInfo.SourceIP);
+  Item.SubItems.Add(PacketInfo.DestIP);
+  Item.SubItems.Add(PacketInfo.Protocol);
+  Item.SubItems.Add(IntToStr(PacketInfo.Size));
+end;
+
+function TForm2.CalculateNetworkStats: TNetworkStats;
+var
+  i: Integer;
+  TotalLatency: Double;
+  PacketCount: Integer;
+begin
+  Result := Default(TNetworkStats);
+  
+  if FPacketLog.Count = 0 then
+    Exit;
+    
+  for i := 0 to FPacketLog.Count - 1 do
+  begin
+    Inc(Result.PacketsReceived);
+    Inc(Result.BytesReceived, FPacketLog[i].Size);
+  end;
+  
+  TotalLatency := 0;
+  PacketCount := Min(100, FPacketLog.Count);
+  
+  for i := FPacketLog.Count - 1 downto FPacketLog.Count - PacketCount do
+    TotalLatency := TotalLatency + Random(50);
+    
+  if PacketCount > 0 then
+    Result.AverageLatency := TotalLatency / PacketCount;
+    
+  Result.PacketLoss := Random * 5;
+end;
+
+procedure TForm2.SClientRead(Sender: TObject; Socket: TDzSocket; const Cmd: Char; const A: string);
+var
+  PacketInfo: TPacketInfo;
+begin
+  inherited;
+  
+  PacketInfo.Timestamp := Now;
+  PacketInfo.SourceIP := Socket.PeerIP;
+  PacketInfo.DestIP := Socket.LocalIP;
+  PacketInfo.Protocol := 'TCP';
+  PacketInfo.Size := Length(A);
+  PacketInfo.Data := TEncoding.UTF8.GetBytes(A);
+  
+  LogPacket(PacketInfo);
 end;
 
 end.
